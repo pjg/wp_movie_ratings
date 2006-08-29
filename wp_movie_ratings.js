157d268dc0e2cb72586ec9485f4d0cabc531ec70
Paweł Gościcki <pawel.goscicki@gmail.com>
2006-08-29 15:30:04 +0000

    Blue-K2 theme problems partially solved.

--- wp_movie_ratings.js
+++ wp_movie_ratings.js
@@ -1,26 +1,34 @@
 // Ask the user before submiting the form
 function delete_confirmation() {
-	if (confirm("You are about to delete this movie rating (including review).\n\nThis cannot be undone. Are you sure?")) return true;
+	if (confirm('You are about to delete this movie rating (including review).\n\nThis cannot be undone. Are you sure?')) return true;
 	else return false;
 }
 
 // Toggle display of movie reviews in page mode
 function toggle_review(id) {
 	var review = document.getElementById(id)
-	var img_array = review.parentNode.getElementsByTagName("img")
+	var img_array = review.parentNode.getElementsByTagName('img')
 	var img = img_array[0]
 
 	if (review && img) {
 		// Show review
-		if (review.style.display == "none") {
-			review.style.display = "block"
-			img.alt = "Hide the review"
+		if (review.style.display == 'none') {
+			if (review.style.setProperty) { 
+				review.style.setProperty('display', 'block', 'important')
+			} else {
+				review.style.display = 'block'
+			}
+			img.alt = 'Hide the review'
 			img.width = 9
 			img.height = 21
 			img.src = img.src.replace(/plus/, 'minus')
 		} else { // Hide review
-			review.style.display = "none"
-			img.alt = "Show the review"
+			if (review.style.setProperty) {
+				review.style.setProperty('display', 'none', 'important')
+			} else {
+				review.style.display = 'none'
+			}
+			img.alt = 'Show the review'
 			img.width = 9
 			img.height = 9
 			img.src = img.src.replace(/minus/, 'plus')
