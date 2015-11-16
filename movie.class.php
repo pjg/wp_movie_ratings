d95ef958d9d7f003d9f7942ca85fe18573c9d7de
Paweł Gościcki <pawel.goscicki@gmail.com>
2015-11-17 00:04:04 +0100

    Add missing `class` to submit button

--- movie.class.php
+++ movie.class.php
@@ -538,7 +538,7 @@ Remember to use correct date format (<code>YYYY-MM-DD HH:MM:SS</code>) when sett
 </table>
 
 <p class="submit">
-<?php if ($action == "Update") echo "<input type=\"submit\" name=\"action\" value=\"Delete movie review &raquo;\" onclick=\"return delete_confirmation()\" />\n"; ?>
+<?php if ($action == "Update") echo "<input class=\"button\" type=\"submit\" name=\"action\" value=\"Delete movie review &raquo;\" onclick=\"return delete_confirmation()\" />\n"; ?>
 <input class="button-primary" type="submit" name="action" value="<?php echo $action ?> movie review &raquo;" />
 </p>
 
