c8cb59b2cb014fd53d84c612612f93ddf53848c4
Paweł Gościcki <pawel.goscicki@gmail.com>
2024-03-25 11:37:11 +0100

    Switch to inlined SVG for plus/minus images
    This should help with fixing script timeout issue for large movie
    reviews pages in recent Wordpress installations (v6.3.3+).
    
    The issue is that Wordpress started doing some image optimizations via
    regexp replace, which requires a lot of processing for documents with
    lots of images (like movie reviews).

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@
 - compatibility with PHP v8
 - support imdb IDs over 100 mln
 - improve rendering performance for large lists when in page mode and sorting movies by `watched_on` date
+- improve/fix rendering time of large lists when in page mode on recent Wordpress versions (6.3.3+) by converting plus/minus images to inlined SVGs
 
 ## Release 1.8 (2019-10-13)
 
--- wp_movie_ratings.js
+++ wp_movie_ratings.js
@@ -7,11 +7,10 @@ function delete_confirmation() {
 // Toggle display of movie reviews in page mode
 function toggle_review(id) {
   var review = document.getElementById(id)
-  var img_array = review.parentNode.getElementsByTagName('img')
-  var img_plus = img_array[0]
-  var img_minus = img_array[1]
+  var expand = review.parentNode.querySelector('.expand')
+  var collapse = review.parentNode.querySelector('.collapse')
 
-  if (review && img_plus && img_minus) {
+  if (review && expand && collapse) {
     // Show the review
     if (review.style.display == 'none') {
       if (review.style.setProperty) {
@@ -20,9 +19,8 @@ function toggle_review(id) {
         review.style.display = 'block'
       }
 
-      img_plus.style.display = 'none'
-        img_minus.style.display = 'block'
-
+      expand.style.display = 'none'
+      collapse.style.display = 'block'
     } else { // Hide the review
       if (review.style.setProperty) {
         review.style.setProperty('display', 'none', 'important')
@@ -30,8 +28,8 @@ function toggle_review(id) {
         review.style.display = 'none'
       }
 
-      img_minus.style.display = 'none'
-        img_plus.style.display = 'block'
+      collapse.style.display = 'none'
+      expand.style.display = 'block'
     }
   }
 }
